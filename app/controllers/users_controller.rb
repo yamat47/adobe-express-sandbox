9cdab15a0e1e884ac5c92cddf16680cf49c2b1ed
yamat47 <yamat47.thirddown@gmail.com>
2025-01-27 16:28:59 +0900

    Enable avatar.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :name ])
+      params.expect(user: [ :name, :avatar ])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
   validates :name, presence: true, uniqueness: true
 end
